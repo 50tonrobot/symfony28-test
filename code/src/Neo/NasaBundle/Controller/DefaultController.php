f345a4f47d82a63da1c0115f321fd933d1e7e2f4
50tonrobot <50tonrobot@gmail.com>
2017-04-18 01:20:33 -0400

    added Neo/NasaBundle

---
+++ code/src/Neo/NasaBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Neo\NasaBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('NasaBundle:Default:index.html.twig');
+    }
+}
